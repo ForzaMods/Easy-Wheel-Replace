c2c21f45c17e886911e0bd66ea34eb3c4ff20755
comamnds <comamnds@outlook.com>
2022-01-09 00:29:47 +0000

    Add project files.

---
+++ Easy Wheel Replace/EasyWheelReplace.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Easy_Wheel_Replace
+{
+    partial class EasyWheelReplace
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFH5Folder = new System.Windows.Forms.OpenFileDialog();
+            this.BTN_GameFolder = new System.Windows.Forms.Button();
+            this.TXT_Gamepath = new System.Windows.Forms.TextBox();
+            this.LST_WantedWheels = new System.Windows.Forms.ComboBox();
+            this.LST_TargetWheels = new System.Windows.Forms.ComboBox();
+            this.BTN_ReplaceWheels = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // OpenFH5Folder
+            // 
+            this.OpenFH5Folder.Title = "Select Any File In the FH5 Game Folder";
+            // 
+            // BTN_GameFolder
+            // 
+            this.BTN_GameFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.BTN_GameFolder.FlatAppearance.BorderSize = 0;
+            this.BTN_GameFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_GameFolder.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BTN_GameFolder.Location = new System.Drawing.Point(12, 13);
+            this.BTN_GameFolder.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.BTN_GameFolder.Name = "BTN_GameFolder";
+            this.BTN_GameFolder.Size = new System.Drawing.Size(310, 67);
+            this.BTN_GameFolder.TabIndex = 0;
+            this.BTN_GameFolder.Text = "Select Game Location";
+            this.BTN_GameFolder.UseVisualStyleBackColor = false;
+            this.BTN_GameFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TXT_Gamepath
+            // 
+            this.TXT_Gamepath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.TXT_Gamepath.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TXT_Gamepath.ForeColor = System.Drawing.Color.White;
+            this.TXT_Gamepath.Location = new System.Drawing.Point(12, 100);
+            this.TXT_Gamepath.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.TXT_Gamepath.Name = "TXT_Gamepath";
+            this.TXT_Gamepath.ReadOnly = true;
+            this.TXT_Gamepath.Size = new System.Drawing.Size(310, 27);
+            this.TXT_Gamepath.TabIndex = 1;
+            // 
+            // LST_WantedWheels
+            // 
+            this.LST_WantedWheels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.LST_WantedWheels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LST_WantedWheels.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LST_WantedWheels.ForeColor = System.Drawing.Color.White;
+            this.LST_WantedWheels.FormattingEnabled = true;
+            this.LST_WantedWheels.Location = new System.Drawing.Point(12, 147);
+            this.LST_WantedWheels.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.LST_WantedWheels.Name = "LST_WantedWheels";
+            this.LST_WantedWheels.Size = new System.Drawing.Size(310, 28);
+            this.LST_WantedWheels.TabIndex = 2;
+            this.LST_WantedWheels.Text = "Wanted Wheels";
+            this.LST_WantedWheels.SelectedIndexChanged += new System.EventHandler(this.LST_WantedWheels_SelectedIndexChanged);
+            // 
+            // LST_TargetWheels
+            // 
+            this.LST_TargetWheels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.LST_TargetWheels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LST_TargetWheels.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LST_TargetWheels.ForeColor = System.Drawing.Color.White;
+            this.LST_TargetWheels.FormattingEnabled = true;
+            this.LST_TargetWheels.Location = new System.Drawing.Point(12, 195);
+            this.LST_TargetWheels.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.LST_TargetWheels.Name = "LST_TargetWheels";
+            this.LST_TargetWheels.Size = new System.Drawing.Size(310, 28);
+            this.LST_TargetWheels.TabIndex = 3;
+            this.LST_TargetWheels.Text = "Target Wheels";
+            this.LST_TargetWheels.SelectedIndexChanged += new System.EventHandler(this.LST_TargetWheels_SelectedIndexChanged);
+            // 
+            // BTN_ReplaceWheels
+            // 
+            this.BTN_ReplaceWheels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.BTN_ReplaceWheels.FlatAppearance.BorderSize = 0;
+            this.BTN_ReplaceWheels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_ReplaceWheels.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BTN_ReplaceWheels.Location = new System.Drawing.Point(12, 243);
+            this.BTN_ReplaceWheels.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.BTN_ReplaceWheels.Name = "BTN_ReplaceWheels";
+            this.BTN_ReplaceWheels.Size = new System.Drawing.Size(310, 67);
+            this.BTN_ReplaceWheels.TabIndex = 4;
+            this.BTN_ReplaceWheels.Text = "Replace Wheels";
+            this.BTN_ReplaceWheels.UseVisualStyleBackColor = false;
+            this.BTN_ReplaceWheels.Click += new System.EventHandler(this.BTN_ReplaceWheels_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Enabled = false;
+            this.ProgressBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ProgressBar.Location = new System.Drawing.Point(12, 323);
+            this.ProgressBar.MarqueeAnimationSpeed = 10;
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(310, 34);
+            this.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.ProgressBar.TabIndex = 5;
+            // 
+            // EasyWheelReplace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(334, 369);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.BTN_ReplaceWheels);
+            this.Controls.Add(this.LST_TargetWheels);
+            this.Controls.Add(this.LST_WantedWheels);
+            this.Controls.Add(this.TXT_Gamepath);
+            this.Controls.Add(this.BTN_GameFolder);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "EasyWheelReplace";
+            this.Text = "Easy Wheel Swap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFH5Folder;
+        private System.Windows.Forms.Button BTN_GameFolder;
+        private System.Windows.Forms.TextBox TXT_Gamepath;
+        private System.Windows.Forms.ComboBox LST_WantedWheels;
+        private System.Windows.Forms.ComboBox LST_TargetWheels;
+        private System.Windows.Forms.Button BTN_ReplaceWheels;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+    }
+}
+
---
+++ Easy Wheel Replace/EasyWheelReplace.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+
+namespace Easy_Wheel_Replace
+{
+    public partial class EasyWheelReplace : Form
+    {
+        public EasyWheelReplace()
+        {
+            InitializeComponent();
+        }
+        string GamePath;
+        bool PathCorrect = false;
+        string[] WantedWheelsList;
+        string[] TargetWheelsList;
+        int wantedwheelsindex = 69420;
+        int targetedwheelsindex  = 69420;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PathCorrect==false)
+            {
+                OpenFH5Folder.ShowDialog();
+                GamePath = new FileInfo(OpenFH5Folder.FileName).DirectoryName;
+                if (File.Exists(GamePath + "\\ForzaHorizon5.exe"))
+                {
+                    TXT_Gamepath.Text = new FileInfo(OpenFH5Folder.FileName).DirectoryName;
+                    PathCorrect = true;
+                    PopulateDropdown();
+                }
+                else
+                {
+                    PathCorrect = false;
+                    MessageBox.Show("Folder selected does not contain the ForzaHorizon5.exe file \nTry again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else
+                MessageBox.Show("Game Folder already selected", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+        private void PopulateDropdown()
+        {
+            WantedWheelsList = Directory.GetFiles(GamePath + "\\media\\cars");
+            for (int i = 0;i < WantedWheelsList.Length;i++)
+            {
+                var sections = (WantedWheelsList[i].Split('\\'));
+                var fileName = sections[sections.Length - 1];
+                LST_WantedWheels.Items.Add(fileName);
+            }
+            TargetWheelsList = Directory.GetFiles(GamePath + "\\media\\cars\\_library\\scene\\wheels");
+            for (int i = 0; i < TargetWheelsList.Length; i++)
+            {
+                var sections = (TargetWheelsList[i].Split('\\'));
+                var fileName = sections[sections.Length - 1];
+                LST_TargetWheels.Items.Add(fileName);
+            }
+        }
+
+        private void BTN_ReplaceWheels_Click(object sender, EventArgs e)
+        {
+            ProgressBar.Enabled = true;
+            if (targetedwheelsindex != 69420 && wantedwheelsindex != 69420)
+            {
+                if(Directory.Exists(@"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\Temp"))
+                {
+                    swapwheels();
+                }
+                else
+                {
+                    Directory.CreateDirectory(@"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper");
+                    Directory.CreateDirectory(@"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\Temp");
+                    Directory.CreateDirectory(@"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\OriginalWheelBackup");
+                    swapwheels();
+                }
+            }
+            else
+                MessageBox.Show("Options not selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void LST_WantedWheels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wantedwheelsindex = LST_WantedWheels.SelectedIndex;
+        }
+
+        private void LST_TargetWheels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            targetedwheelsindex = LST_TargetWheels.SelectedIndex;
+        }
+        void swapwheels()
+        {
+            var wantedwheelpath = WantedWheelsList[wantedwheelsindex];
+            var targetwheelpath = TargetWheelsList[targetedwheelsindex];
+            var targetmedianame = LST_TargetWheels.SelectedItem.ToString();
+            var wantedmedianame = LST_WantedWheels.SelectedItem.ToString();
+            string tempfolder = @"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\Temp\";
+            //TXT_Progress.Items.Add("Replacing wheel: " + LST_TargetWheels.SelectedItem + " with wheel: " + LST_WantedWheels.SelectedItem);
+            //TXT_Progress.Items.Add("Backing up original wheel zip to: " + @"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\OriginalWheelBackup\" + LST_TargetWheels.SelectedItem);
+            if (!File.Exists(@"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\OriginalWheelBackup\" + LST_TargetWheels.SelectedItem))
+                File.Copy(targetwheelpath, @"C:\Users\" + Environment.UserName + @"\Documents\EasyWheelSwapper\OriginalWheelBackup\" + LST_TargetWheels.SelectedItem);
+            //TXT_Progress.Items.Add("Moving wheels to " + tempfolder);
+            File.Copy(wantedwheelpath, tempfolder + LST_WantedWheels.SelectedItem);
+            File.Move(targetwheelpath, tempfolder + LST_TargetWheels.SelectedItem);
+            //TXT_Progress.Items.Add("Extracting Wheel zips to: " + tempfolder);
+            ZipFile.ExtractToDirectory(tempfolder + LST_WantedWheels.SelectedItem, tempfolder + @"\W\");
+            ZipFile.ExtractToDirectory(tempfolder + LST_TargetWheels.SelectedItem, tempfolder + @"\T\");
+            Directory.CreateDirectory(tempfolder + @"\N\");
+            Directory.CreateDirectory(tempfolder + @"\N\Textures");
+            Directory.CreateDirectory(tempfolder + @"\N\Textures\AO\");
+            //TXT_Progress.Items.Add("Renaming and replacing wheel files");
+            File.Move(tempfolder + @"\W\scene\_library\scene\Wheels\" + wantedmedianame.Substring(0, wantedmedianame.Length - 4) + "_wheelLF.modelbin", tempfolder + @"\N\" + targetmedianame.Substring(0, targetmedianame.Length - 4) + "_wheelLF.modelbin");
+
+            if (File.Exists(tempfolder + @"\W\Textures\AO\" + wantedmedianame.Substring(0, wantedmedianame.Length - 4) + "_wheelLF_AO.swatchbin"))
+            {
+                File.Move(tempfolder + @"\W\Textures\AO\" + wantedmedianame.Substring(0, wantedmedianame.Length - 4) + "_wheelLF_AO.swatchbin", tempfolder + @"\N\Textures\AO\" + targetmedianame.Substring(0, targetmedianame.Length - 4) + "_wheelLF_AO.swatchbin");
+            }
+            else
+            {
+                var found = false;
+                //TXT_Progress.Items.Add("Texture not present in car zip. Searching Textures.zip");
+                using (ZipArchive archive = ZipFile.OpenRead(GamePath + @"\media\cars\_library\Textures.zip"))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.FullName.Contains(wantedmedianame.Substring(0, wantedmedianame.Length - 4) + "_wheellf", StringComparison.OrdinalIgnoreCase))
+                        {
+                            archive.GetEntry(entry.FullName).ExtractToFile(tempfolder + @"\N\Textures\AO\" + targetmedianame.Substring(0, targetmedianame.Length - 4) + "_wheelLF_AO.swatchbin");
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if (!found)
+                {
+                    //TXT_Progress.Items.Add("Texture not present in Textures.zip. Searching Textures_pri_45.zip");
+                    using (ZipArchive archive = ZipFile.OpenRead(GamePath + @"\media\cars\_library\Textures_pri_45.zip"))
+                    {
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            if (entry.FullName.Contains(wantedmedianame.Substring(0, wantedmedianame.Length - 4) + "_wheellf", StringComparison.OrdinalIgnoreCase))
+                            {
+                                archive.GetEntry(entry.FullName).ExtractToFile(tempfolder + @"\N\Textures\AO\" + targetmedianame.Substring(0, targetmedianame.Length - 4) + "_wheelLF_AO.swatchbin");
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            //TXT_Progress.Items.Add("Creating new zip to replace: " + targetwheelpath);
+            ZipFile.CreateFromDirectory(tempfolder + @"\N\", targetwheelpath);
+
+
+            //TXT_Progress.Items.Add("Deleting contents of: " + tempfolder);
+            Directory.Delete(tempfolder, true);
+            Directory.CreateDirectory(tempfolder);
+            ProgressBar.Style = ProgressBarStyle.Continuous;
+        }
+    }
+}
